814361398460837f0b6ecbc82cd4dcff389211eb
briankalwat <brian@briankalwat.com>
2018-05-21 17:23:05 -0400

    added card sliders and accordion partials, updated more styles

---
+++ js/main.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+/*
+
+CARD SLIDERS
+
+*/
+
+// Get all sliders on page
+var sliders = document.querySelectorAll('[data-slider]');
+
+// Loop through them and adjust container width
+sliders.forEach(function(element) {
+
+    var cardWidth = "300";
+
+    // Set slider element
+    var slider = element.children[0];
+
+    // Calculate count of slider child elements
+    var sliderItemCount = slider.children.length;
+
+    // Set child elements array
+    var sliderChildren = slider.children
+    
+    var index;
+
+    // Loop through child elements array and set widths
+    for (i = 0; i < sliderChildren.length; ++i) {
+        sliderChildren[i].style.width = cardWidth - 30 + "px";
+    }
+
+    // Calculate slider width
+    var sliderWrapperWidth = sliderItemCount * cardWidth - 30;
+
+    // Adjust slider width
+    slider.style.width = sliderWrapperWidth + "px";
+
+});
+
+
+});
